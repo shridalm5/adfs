213367ec52a43844ce829239aa4ec32b7505be0b
shridalm5 <shridalm@gmail.com>
2021-05-20 18:46:00 +0530

    Shrikant

--- db.php
+++ db.php
@@ -1,6 +1,8 @@
 <?php
     // Enter your host name, database username, password, and database name.
     // If you have not set database password on localhost then set empty.
+    
+    //change
     $con = mysqli_connect("localhost","root","Tyrion@00","LoginSystem");
     // Check connection
     if (mysqli_connect_errno()){
