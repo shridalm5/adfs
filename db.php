0fed71a4997e042419602c41f2ad570c789fa4a4
Dipak Miglani <dipakm@iitk.ac.in>
2021-05-20 18:14:03 +0530

    password change

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
     // Enter your host name, database username, password, and database name.
     // If you have not set database password on localhost then set empty.
-    $con = mysqli_connect("localhost","root","7073501651","LoginSystem");
+    $con = mysqli_connect("localhost","root","Tyrion@00","LoginSystem");
     // Check connection
     if (mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
